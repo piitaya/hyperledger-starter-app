5c20c12ed4cd13ac5a03988be4e9f2ca4045ed79
Paul Bottein <pbottein@fr.ibm.com>
2016-06-14 17:16:11 +0200

    Continue monitor component (design)

--- client/app/monitor/monitor.component.js
+++ client/app/monitor/monitor.component.js
@@ -13,11 +13,26 @@
 	function MonitorComponent() {
 		var vm = this;
     vm.open = false;
-    
+    vm.items = [];
+
+    vm.add = add;
+    vm.remove = remove;
 		activate();
 
 		function activate() {
-
+        for (var i=1; i <= 5; i++) {
+            vm.items.push(i);
+        }
 		}
+
+    function add(e) {
+        vm.items.push(vm.items.length + 1);
+        e.stopPropagation();
+    }
+
+    function remove(e) {
+        vm.items.splice(vm.items.length - 1, 1);
+        e.stopPropagation();
+    }
 	}
 })();
